850c790febc14e06388d58d3131172816c836cb2
Klajdi Kolaj <kljdkolaj@gmail.com>
2026-05-19 09:49:24 +0200

    Normalize npm bin metadata

--- Formula/upwork-autopilot.rb
+++ Formula/upwork-autopilot.rb
@@ -5,7 +5,7 @@ class UpworkAutopilot < Formula
   desc "Codex plugin for controlled Upwork application sessions"
   homepage "https://github.com/klajdikkolaj/upwork-autopilot"
   url "https://registry.npmjs.org/upwork-autopilot/-/upwork-autopilot-0.4.0.tgz"
-  sha256 "56c5b8b5d57a953fd89ce5105d7cec1ea5252cd8807ded91fd850a14034e61b4"
+  sha256 "82677826053765c53dedea71696f14db47723e97fc0092943cde690e177651a3"
   license "MIT"
 
   depends_on "node"
